8bc6b032d95814864e7f267a6dea55c78508078a
RubixDev <silas.groh@t-online.de>
2025-01-30 20:47:23 +0100

    chore: move render stuff to extra module

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use ansi_to_tui::IntoText;
 use anyhow::{Context as _, Result};
 use average::Averaged;
 use bluetooth_serial_port::BtAddr;
-use chrono::{DateTime, Local};
+use chrono::Local;
 use crossbeam::queue::ArrayQueue;
 use crossterm::event::{self, Event, KeyCode};
-use image::{DynamicImage, GenericImage, Rgb, RgbImage};
+use image::DynamicImage;
 use libpulse_binding::volume::Volume;
 use log::{debug, error, info, trace};
 use pixoo::{Brightness, Pixoo, DISPLAY_SIZE};
@@ -19,11 +19,13 @@ use ratatui::{
     widgets::{Block, BorderType, Padding, Paragraph},
     DefaultTerminal,
 };
+use render::Context;
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, Networks, RefreshKind, System};
 use tui_logger::{LevelFilter, TuiLoggerSmartWidget, TuiWidgetEvent, TuiWidgetState};
 
 mod average;
 mod fonts;
+mod render;
 
 fn main() {
     tui_logger::init_logger(LevelFilter::Trace)
@@ -48,6 +50,7 @@ const NETWORK_INTERFACE: &str = "enp37s0";
 
 const PROGRESS_STEPS: u8 = 3;
 const PROGRESS_RANGE: f64 = 15. * PROGRESS_STEPS as f64;
+const MAX_NETWORK: f64 = 200_000.;
 
 fn try_main(terminal: &mut DefaultTerminal) -> Result<()> {
     let mut log_state = TuiWidgetState::new()
@@ -109,8 +112,8 @@ fn try_main(terminal: &mut DefaultTerminal) -> Result<()> {
         let net = networks
             .get(NETWORK_INTERFACE)
             .context("get network interface")?;
-        let net_up = net_up_data.next(net.transmitted() as f64, 200_000.);
-        let net_down = net_down_data.next(net.received() as f64, 200_000.);
+        let net_up = net_up_data.next(net.transmitted() as f64, MAX_NETWORK);
+        let net_down = net_down_data.next(net.received() as f64, MAX_NETWORK);
 
         let ctx = Context {
             cpu,
@@ -123,7 +126,7 @@ fn try_main(terminal: &mut DefaultTerminal) -> Result<()> {
             time: Local::now(),
         };
         trace!("updated context: {ctx:?}");
-        let img = DynamicImage::from(create_frame(ctx));
+        let img = DynamicImage::from(render::create_frame(ctx));
         let ansi_img = ansipix::of_image(
             &img,
             (DISPLAY_SIZE as usize, DISPLAY_SIZE as usize),
@@ -210,155 +213,5 @@ fn pixoo_worker(rx: Arc<ArrayQueue<DynamicImage>>) -> Result<()> {
             trace!("sending new image to device");
             pixoo.set_image(&img).context("sending frame")?;
         }
-    }
-}
-
-#[derive(Debug)]
-struct Context {
-    cpu: u8,
-    mem: u8,
-    gpu: u8,
-    gpu_mem: u8,
-    vol: u8,
-    net_up: u8,
-    net_down: u8,
-    time: DateTime<Local>,
-}
-
-fn create_frame(ctx: Context) -> RgbImage {
-    let mut img = RgbImage::new(DISPLAY_SIZE, DISPLAY_SIZE);
-
-    draw_progress(
-        ctx.vol,
-        &mut img,
-        0,
-        [
-            Rgb([0xff, 0xaa, 0x00]),
-            Rgb([0xbb, 0x88, 0x00]),
-            Rgb([0x88, 0x55, 0x00]),
-        ],
-    );
-    draw_progress(
-        ctx.mem,
-        &mut img,
-        1,
-        [
-            Rgb([0x00, 0xdd, 0x00]),
-            Rgb([0x00, 0x99, 0x00]),
-            Rgb([0x00, 0x55, 0x00]),
-        ],
-    );
-    draw_progress(
-        ctx.cpu,
-        &mut img,
-        2,
-        [
-            Rgb([0x00, 0x00, 0xdd]),
-            Rgb([0x00, 0x00, 0x99]),
-            Rgb([0x00, 0x00, 0x55]),
-        ],
-    );
-    draw_progress(
-        ctx.gpu,
-        &mut img,
-        3,
-        [
-            Rgb([0xff, 0x77, 0x00]),
-            Rgb([0xbb, 0x55, 0x00]),
-            Rgb([0x88, 0x33, 0x00]),
-        ],
-    );
-    draw_progress(
-        ctx.gpu_mem,
-        &mut img,
-        4,
-        [
-            Rgb([0xdd, 0x00, 0x00]),
-            Rgb([0x99, 0x00, 0x00]),
-            Rgb([0x55, 0x00, 0x00]),
-        ],
-    );
-    draw_progress(
-        ctx.net_up,
-        &mut img,
-        5,
-        [
-            Rgb([0x00, 0x99, 0xff]),
-            Rgb([0x00, 0x77, 0xbb]),
-            Rgb([0x00, 0x55, 0x88]),
-        ],
-    );
-    draw_progress(
-        ctx.net_down,
-        &mut img,
-        6,
-        [
-            Rgb([0x99, 0x00, 0xff]),
-            Rgb([0x66, 0x00, 0x99]),
-            Rgb([0x44, 0x00, 0x66]),
-        ],
-    );
-
-    write_string(
-        &ctx.time.format("%l:%M").to_string(),
-        &mut img,
-        (3, 7),
-        &[Rgb([0xff, 0x00, 0xff]), Rgb([0xff, 0x00, 0x99])],
-        &fonts::FONT_3X5,
-        false,
-    );
-    write_string(
-        &ctx.time.format("%S").to_string(),
-        &mut img,
-        (10, 12),
-        &[Rgb([0x88, 0x00, 0x88]), Rgb([0x88, 0x00, 0x55])],
-        &fonts::FONT_3X4,
-        false,
-    );
-
-    img
-}
-
-fn draw_progress<I: GenericImage>(progress: u8, img: &mut I, y: u32, colors: [I::Pixel; 3]) {
-    let full = (progress / PROGRESS_STEPS).min(15);
-    let rest = match full >= 15 {
-        true => 0,
-        false => progress % PROGRESS_STEPS,
-    };
-    for x in 0..=full {
-        img.put_pixel(x as u32, y, colors[0]);
-    }
-    match rest {
-        1 => img.put_pixel(full as u32 + 1, y, colors[2]),
-        2 => img.put_pixel(full as u32 + 1, y, colors[1]),
-        _ => {}
-    }
-}
-
-fn write_string<I: GenericImage>(
-    str: &str,
-    img: &mut I,
-    (mut x, y): (u32, u32),
-    colors: &[I::Pixel],
-    font: &phf::Map<char, &[&[bool]]>,
-    spaces: bool,
-) {
-    let mut i = 0;
-    for char in str.chars() {
-        let Some(&char) = font.get(&char) else {
-            continue;
-        };
-
-        for (cy, &line) in char.iter().enumerate() {
-            for (cx, &px) in line.iter().enumerate() {
-                if px {
-                    img.put_pixel(x + cx as u32, y + cy as u32, colors[i]);
-                }
-            }
-        }
-
-        x += char[0].len() as u32 + spaces as u32;
-        i += 1;
-        i %= colors.len();
     }
 }
---
+++ src/render.rs
@@ -0,0 +1,155 @@
+use chrono::{DateTime, Local};
+use image::{GenericImage, Rgb, RgbImage};
+use pixoo::DISPLAY_SIZE;
+
+use crate::{fonts, PROGRESS_STEPS};
+
+#[derive(Debug)]
+pub struct Context {
+    pub cpu: u8,
+    pub mem: u8,
+    pub gpu: u8,
+    pub gpu_mem: u8,
+    pub vol: u8,
+    pub net_up: u8,
+    pub net_down: u8,
+    pub time: DateTime<Local>,
+}
+
+pub fn create_frame(ctx: Context) -> RgbImage {
+    let mut img = RgbImage::new(DISPLAY_SIZE, DISPLAY_SIZE);
+
+    draw_progress(
+        ctx.vol,
+        &mut img,
+        0,
+        [
+            Rgb([0xff, 0xaa, 0x00]),
+            Rgb([0xbb, 0x88, 0x00]),
+            Rgb([0x88, 0x55, 0x00]),
+        ],
+    );
+    draw_progress(
+        ctx.mem,
+        &mut img,
+        1,
+        [
+            Rgb([0x00, 0xdd, 0x00]),
+            Rgb([0x00, 0x99, 0x00]),
+            Rgb([0x00, 0x55, 0x00]),
+        ],
+    );
+    draw_progress(
+        ctx.cpu,
+        &mut img,
+        2,
+        [
+            Rgb([0x00, 0x00, 0xdd]),
+            Rgb([0x00, 0x00, 0x99]),
+            Rgb([0x00, 0x00, 0x55]),
+        ],
+    );
+    draw_progress(
+        ctx.gpu,
+        &mut img,
+        3,
+        [
+            Rgb([0xff, 0x77, 0x00]),
+            Rgb([0xbb, 0x55, 0x00]),
+            Rgb([0x88, 0x33, 0x00]),
+        ],
+    );
+    draw_progress(
+        ctx.gpu_mem,
+        &mut img,
+        4,
+        [
+            Rgb([0xdd, 0x00, 0x00]),
+            Rgb([0x99, 0x00, 0x00]),
+            Rgb([0x55, 0x00, 0x00]),
+        ],
+    );
+    draw_progress(
+        ctx.net_up,
+        &mut img,
+        5,
+        [
+            Rgb([0x00, 0x99, 0xff]),
+            Rgb([0x00, 0x77, 0xbb]),
+            Rgb([0x00, 0x55, 0x88]),
+        ],
+    );
+    draw_progress(
+        ctx.net_down,
+        &mut img,
+        6,
+        [
+            Rgb([0x99, 0x00, 0xff]),
+            Rgb([0x66, 0x00, 0x99]),
+            Rgb([0x44, 0x00, 0x66]),
+        ],
+    );
+
+    write_string(
+        &ctx.time.format("%l:%M").to_string(),
+        &mut img,
+        (3, 7),
+        &[Rgb([0xff, 0x00, 0xff]), Rgb([0xff, 0x00, 0x99])],
+        &fonts::FONT_3X5,
+        false,
+    );
+    write_string(
+        &ctx.time.format("%S").to_string(),
+        &mut img,
+        (10, 12),
+        &[Rgb([0x88, 0x00, 0x88]), Rgb([0x88, 0x00, 0x55])],
+        &fonts::FONT_3X4,
+        false,
+    );
+
+    img
+}
+
+fn draw_progress<I: GenericImage>(progress: u8, img: &mut I, y: u32, colors: [I::Pixel; 3]) {
+    let full = (progress / PROGRESS_STEPS).min(15);
+    let rest = match full >= 15 {
+        true => 0,
+        false => progress % PROGRESS_STEPS,
+    };
+    for x in 0..=full {
+        img.put_pixel(x as u32, y, colors[0]);
+    }
+    match rest {
+        1 => img.put_pixel(full as u32 + 1, y, colors[2]),
+        2 => img.put_pixel(full as u32 + 1, y, colors[1]),
+        _ => {}
+    }
+}
+
+fn write_string<I: GenericImage>(
+    str: &str,
+    img: &mut I,
+    (mut x, y): (u32, u32),
+    colors: &[I::Pixel],
+    font: &phf::Map<char, &[&[bool]]>,
+    spaces: bool,
+) {
+    let mut i = 0;
+    for char in str.chars() {
+        let Some(&char) = font.get(&char) else {
+            continue;
+        };
+
+        for (cy, &line) in char.iter().enumerate() {
+            for (cx, &px) in line.iter().enumerate() {
+                if px {
+                    img.put_pixel(x + cx as u32, y + cy as u32, colors[i]);
+                }
+            }
+        }
+
+        x += char[0].len() as u32 + spaces as u32;
+        i += 1;
+        i %= colors.len();
+    }
+}
